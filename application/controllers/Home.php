9de91ec24fdeb409241933f0af63702530c3640f
deen <nuradeen.bing0899@gmail.com>
2021-10-04 17:27:25 +0700

    master

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -12,12 +12,7 @@ class home extends CI_Controller {
 
     public function index()
 	{
-		// $data['cats_list'] = $this->Cart_model->cat_list();
-		// $data['products_sele_list'] = $this->Product_model->products_sele_list();		
-		// $data['reviewies_list'] = $this->Review_model->reviewies_list();
-		
 		$this->load->view('head');
 		$this->load->view('home');
-
 	}
 }
